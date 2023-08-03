5f161de76d01b7ecc3a11893065fe2c8c9764917
AhmAlgiz <a.ahmatzyanov@yandex.ru>
2023-08-03 17:38:15 +0300

    bad config

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,25 @@ import (
 	"github.com/AhmAlgiz/marketplace/pkg/handler"
 	"github.com/AhmAlgiz/marketplace/pkg/repository"
 	"github.com/AhmAlgiz/marketplace/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error reading config file %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	s := new(marketplace.Server)
-	if err := s.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatalf("Server running error: %s", err)
+	if err := s.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("Server running error: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
