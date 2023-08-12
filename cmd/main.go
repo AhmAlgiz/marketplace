f6fb87b1d500bb6cbae703dea9cf011855200132
AhmAlgiz <97907492+AhmAlgiz@users.noreply.github.com>
2023-08-12 09:55:06 +0300

    added postgres connection

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/AhmAlgiz/marketplace"
 	"github.com/AhmAlgiz/marketplace/pkg/handler"
 	"github.com/AhmAlgiz/marketplace/pkg/repository"
 	"github.com/AhmAlgiz/marketplace/pkg/service"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
-		log.Fatalf("Error reading config file %s", err.Error())
+		log.Fatalf("Error reading config file: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize DB: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
